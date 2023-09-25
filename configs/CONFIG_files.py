324ec67843122d1b853ab06a139fe3d0e9a82e82
Hao <107262205+hao72700@users.noreply.github.com>
2023-09-25 11:47:44 -0400

    cwd fix

--- configs/CONFIG_files.py
+++ configs/CONFIG_files.py
@@ -1,12 +1,16 @@
-# File CONFIG
-results_dir = "results"
+import pathlib
+cwd = str(pathlib.Path(__file__).parent.parent.resolve())
+
+# File path CONFIG
+results_dir = f"{cwd}/results"
 simulation_path = f"{results_dir}/simulation.csv"
 initial_path = f"{results_dir}/initial.csv"
 results_path = f"{results_dir}/results.csv"
 params_path = f"{results_dir}/params.csv"
 
-inputs_dir = "inputs"
+inputs_dir = f"{cwd}/inputs"
 screamer_wf_path = f"{inputs_dir}/screamer_wf.csv"
 
 MAX_ITERATIONS = 200000
 use_SCREAMER = False
+
